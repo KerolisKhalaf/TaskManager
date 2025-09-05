2cd49aaa1b587f891be5ac952ce7862a595996af
Kerolis Khalaf <kerokhalaf59@gmail.com>
2025-09-05 19:49:22 +0200

    create basic routes

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
+
+//middleware
+app.use(express.json());
+
+
 
 //routs
 app.get('/hello', (req, res) => {
     res.send('Task Manager App');
-}
+});
 
 
 
-
-
+app.use('/api/v1/tasks', tasks);
 
 
 
