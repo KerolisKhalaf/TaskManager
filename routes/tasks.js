15a373d50283d3f045a574faa6d6e758e93cd67f
Kerolis Khalaf <kerokhalaf59@gmail.com>
2025-09-06 05:07:04 +0200

    intiat database and data schema

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,7 +3,13 @@ const  router =express.Router();
 
 
 
-const {getAllTasks,createTask,getOneTask,updataTask,deleteTask} = require('../controllers/tasks');
+const {
+    getAllTasks,
+    createTask,
+    getOneTask,
+    updataTask
+    ,deleteTask
+} = require('../controllers/tasks');
 
 router.route('/').get(getAllTasks).post(createTask);
 router.route('/:id').get(getOneTask).patch(updataTask).delete(deleteTask);
